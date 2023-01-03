413718c99db507ab8d8406c6b7885a6d531c951c
Student <ashutoshmaha2909@gmail.com>
2023-01-03 12:00:36 +0530

    added files

--- ASSIGNMENT-5/q16.java
+++ ASSIGNMENT-5/q16.java
@@ -1,9 +1,16 @@
 public class q16 {
   static void rotate(int[] arr){
-    // some code
+    int last = arr[arr.length-1];
+    for(int i=arr.length-1; i>0; i--){
+      arr[i] = arr[i-1];
+    }
+    arr[0] = last;
   }
   public static void main(String[] args) {
     int[] arr = { 3242, 43, 46, 43 };
     rotate(arr);
+    for(int x: arr){
+      System.out.print(x+ " ");
+    }
   }
 }
--- ASSIGNMENT-5/q18.java
+++ ASSIGNMENT-5/q18.java
@@ -1,6 +1,28 @@
 // MERGE SORT
 public class q18 {
+  static int[] mergeSort(int[] arr1, int[] arr2){
+    int p1 = 0, p2 = 0;
+    int[] mergeArr = new int[arr1.length + arr2.length];
+    for (int i= 0; i<mergeArr.length;  i++){
+      int x = arr1[p1], y = arr2[p2];
+      if (y<x && p2<arr2.length-1){
+        p2 ++;
+        mergeArr[i] = y;
+      } else {
+        if (p1<arr1.length-1)
+        p1++;
+        mergeArr[i] = x;
+      }
+    }
+    return mergeArr;
+  }
   public static void main(String[] args) {
-    
+    int[] arr1 = {5,43, 45,54};
+    int[] arr2 = {6,45,65, 456,645,6556};
+    int[] arr3 = mergeSort(arr1, arr2);
+    for (int x: arr3){
+      System.out.println(x);
+    }
   }
 }
+ 
